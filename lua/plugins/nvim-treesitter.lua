da5f89d742fb8bec8a501929f2dec5ab8f6f5a95
Alexander Butenko <a.butenko.o@gmail.com>
2024-03-25 00:22:42 +0100

    Add regex and bash ts parsers

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,33 +1,35 @@
 local opts = {
-    ensure_installed = {
-      "vimdoc",
-      "vim",
-      "lua",
-      "python",
-      "c",
-      "haskell",
-      "go",
-      "zig",
-      "rust",
-      "json",
-      "markdown",
-      "markdown_inline",
-      "dockerfile",
-      "sql",
-    },
-    highlight = {
-      enable = true,
-      use_languagetree = true,
-    },
-    indent = { enable = true },
-    auto_install = true,
-    autotag = { enable = true }, -- Close and rename html tags
-  }
+	ensure_installed = {
+		"vimdoc",
+		"vim",
+		"lua",
+		"python",
+		"c",
+		"haskell",
+		"go",
+		"zig",
+		"rust",
+		"json",
+		"markdown",
+		"markdown_inline",
+		"dockerfile",
+		"sql",
+		"bash",
+		"regex",
+	},
+	highlight = {
+		enable = true,
+		use_languagetree = true,
+	},
+	indent = { enable = true },
+	auto_install = true,
+	autotag = { enable = true }, -- Close and rename html tags
+}
 
 return {
-  "nvim-treesitter/nvim-treesitter",
-  lazy = false,
-  config = function()
-    require("nvim-treesitter.configs").setup(opts)
-  end,
+	"nvim-treesitter/nvim-treesitter",
+	lazy = false,
+	config = function()
+		require("nvim-treesitter.configs").setup(opts)
+	end,
 }
